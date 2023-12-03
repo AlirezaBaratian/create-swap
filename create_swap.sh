a4a5c084ff01ee2acc7b04b2d24b9710e587bb31
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-03 14:19:06 +0330

    feat: Add comments - Removed extra prints

--- create_swap.sh
+++ create_swap.sh
@@ -4,19 +4,17 @@ function main() {
     clear
     echo "👊🏼 This script allocates a swap partition and makes it persistant after reboots."
     read -p "🤔 How much swap do you want in GB? " amount
-
+    
+    # Create a swapfile in / and set permissions
     sudo fallocate -l ${amount}G /swapfile
-    ls -lh /swapfile
-
     sudo chmod 600 /swapfile
-    ls -lh /swapfile
 
+    # Make a swap partition out
     sudo mkswap /swapfile
     sudo swapon /swapfile
     sudo swapon --show
 
-    free -h
-
+    # Apply the setting for reboots
     sudo cp /etc/fstab /etc/fstab.bak
     echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
