ab0c88416de98ae9a169875a38ff6c51be2c8fe2
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-23 15:18:07 +0330

    feat: clear commnad outputs

--- create_swap.sh
+++ create_swap.sh
@@ -6,13 +6,12 @@ function main() {
     read -p "🤔 How much swap do you want in GB? " amount
     
     # Create a swapfile in / and set permissions
-    sudo fallocate -l ${amount}G /swapfile
+    sudo fallocate -l ${amount}G /swapfile > /dev/null
     sudo chmod 600 /swapfile
 
     # Make a swap partition out
-    sudo mkswap /swapfile
-    sudo swapon /swapfile
-    sudo swapon --show
+    sudo mkswap /swapfile > /dev/null
+    sudo swapon /swapfile > /dev/null
 
     # Apply the setting for reboots
     sudo cp /etc/fstab /etc/fstab.bak
